964792cbd68e8b38b8c87ea05e6100238e5d5775
gouthm.04 <gouthm.04@gmail.com>
2026-07-11 14:45:13 +0530

    added view course page

---
+++ src/components/ViewCourse.jsx
@@ -0,0 +1,62 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const ViewCourse = () => {
+    const [data,changeData] = useState([])
+
+    const fetchData = () => {
+        axios.get("http://192.168.33.245:5001/api/courses")
+        .then(
+            (response) => {
+                changeData(response.data)
+            },[]
+        ).catch()
+    }
+
+    useEffect(
+        () => {
+            fetchData()
+        },[]
+    )
+  return (
+    <div>
+        <div className="container mt-3">
+            <div className="row">
+                <div className="col-12">
+                   <table class="table table-hover">
+                        <thead className='table-dark'>
+                            <tr>
+                            <th scope="col">Course Name</th>
+                            <th scope="col">Duration</th>
+                            <th scope="col">Fee</th>
+                            <th scope="col">Mode</th>
+                            <th scope="col">Trainer</th>
+                            <th scope="col">Created At</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                           
+                           {data.map(
+                                (value,index) => {
+                                    return (
+                                         <tr>
+                                            <th scope="row">{value.course_name}</th>
+                                            <td>{value.duration}</td>
+                                            <td>{value.fee}</td>
+                                            <td>{value.mode}</td>
+                                            <td>{value.trainer}</td>
+                                            <td>{value.created_at}</td>
+                                        </tr>
+                                    )
+                                }
+                           )}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ViewCourse
